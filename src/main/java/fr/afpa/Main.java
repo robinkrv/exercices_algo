9fba9f6b45c4a1b7b7f37a014d1f4d1079fb5f23
robinkrv <163308237+robinkrv@users.noreply.github.com>
2025-04-17 08:55:58 +0200

    Fin du niveau 1

--- src/main/java/fr/afpa/Main.java
+++ src/main/java/fr/afpa/Main.java
@@ -2,26 +2,66 @@ package fr.afpa;
 
 import fr.afpa.exercices.NiveauUn;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-import static fr.afpa.exercices.NiveauUn.isMinMax;
+import static fr.afpa.exercices.NiveauUn.*;
 
 public class Main {
     public static void main(String[] args) {
 
         //NiveauUn niveauUn = new NiveauUn();
-        //NiveauUn.checkAge();
-        //NiveauUn.checkVowelConsonant();
-        //NiveauUn.doYouHaveEnuff();
-        //NiveauUn.calculTauxParticipation();
-        //NiveauUn.subscribersCount(276,1.04);
-        //NiveauUn.sommeNb();
-        //NiveauUn.fizzBuzzing();
-        //NiveauUn.calculFactoriel(18);
-        //NiveauUn.tableauReel();
+        //checkAge();
+        //checkVowelConsonant();
+        //doYouHaveEnuff();
+        //calculTauxParticipation();
+        //subscribersCount(276,1.04);
+        //sommeNb();
+        //fizzBuzzing();
+        //calculFactoriel(18);
+        //tableauReel();
 
 
+        //int index = rechercheFor(new int[] {12, 11, 4, 2},2);
+        //System.out.println(index);
+
+        //int[] tab = {4, 6, 2, 2};
+        //System.out.println(sommeIndex(tab, 1, 3));
+
+        //int[] result = findMinMaxIndexes(new int[] {12, 11, 4, 2});
+        //System.out.println(Arrays.toString(result));
+
+        //int[] array = {4, 6, 2, 155};
+
+        //boolean success = swap(array, 1, 3);
+        //System.out.println("Échange effectué : " + success);
+        //System.out.println("Nouveau tableau : " + java.util.Arrays.toString(array));
+
+        //int[] array = {4, 6, 2, 155};
+
+        //System.out.println(calculerMoyenne(array));
+
+        //tableManip();
+
+        //invert(array);
+
+        //int[][] array2D = {{1,2},{3,5,4},{56,8,2}};
+
+        //sommeTableau2D(array2D);
+
+        //int[][] array2D = {
+        //   {4, 6, 5, 2},
+        //   {7, 11, 34, 1},
+        //   {28, 6, 2, 15},
+        //   {1, 2, 3, 19}
+        //};
+
+        //sumDiagonal(array2D,4,4,1);
+
+        //doCharTriangle();
+        runPlusOuMoinsGame();
+    }
 
 
     }
-}
+
--- src/main/java/fr/afpa/exercices/NiveauUn.java
+++ src/main/java/fr/afpa/exercices/NiveauUn.java
@@ -1,9 +1,9 @@
 package fr.afpa.exercices;
 
 import java.time.Year;
-import java.time.YearMonth;
 import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class NiveauUn {
@@ -53,10 +53,9 @@ public class NiveauUn {
 
         if (var1 >= var2 && var1 >= var3) {
             max = var1;
-        } else if (var2 >= var1 && var2 >= var3){
+        } else if (var2 >= var1 && var2 >= var3) {
             max = var2;
-        }
-        else {
+        } else {
             max = var3;
         }
 
@@ -64,13 +63,13 @@ public class NiveauUn {
             min = var1;
         } else if (var2 <= var1 && var2 <= var3) {
             min = var2;
-        }
-        else {
+        } else {
             min = var3;
         }
         System.out.println("Le chiffre le plus petit est : " + min);
         System.out.println("Le chiffre le plus grand est : " + max);
     }
+
     public static void checkMinMax() {
         Scanner scanner = new Scanner(System.in);
 
@@ -88,17 +87,16 @@ public class NiveauUn {
 
     public static void isVowelOrConsonant(char c) {
 
-    if (!Character.isAlphabetic(c)) {
-        System.out.println("Vous n'avez pas entré une lettre");
-        return;
-    }
+        if (!Character.isAlphabetic(c)) {
+            System.out.println("Vous n'avez pas entré une lettre");
+            return;
+        }
 
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'y') {
-        System.out.println("Votre lettre est une voyelle");
-    }
-    else {
-        System.out.println("Votre lettre est une console");
-    }
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'y') {
+            System.out.println("Votre lettre est une voyelle");
+        } else {
+            System.out.println("Votre lettre est une console");
+        }
     }
 
     public static void checkVowelConsonant() {
@@ -120,16 +118,15 @@ public class NiveauUn {
         double price = scanner.nextDouble();
 
         if (bankSum > price) {
-            System.out.println("Vous pouvez acheter ! Il vous reste " + String.format("%.2f", (bankSum - price)) + " sur votre compte" );
-        }
-        else {
+            System.out.println("Vous pouvez acheter ! Il vous reste " + String.format("%.2f", (bankSum - price)) + " sur votre compte");
+        } else {
             System.out.println("T'as pas de sous fréro");
         }
     }
 
     public static void calculTauxParticipation() {
 
-        //Constantes
+
         final double TAUX_CELIB = 20.0;
         final double TAUX_AUTRE = 25.0;
         final double MAJ_ENFANT_SUPPL = 15.0;
@@ -139,7 +136,7 @@ public class NiveauUn {
 
         Scanner scanner = new Scanner(System.in);
         String continuer;
-        // Déclaration des variables
+
         String statut;
         int nbEnfants;
         double salaire;
@@ -158,8 +155,7 @@ public class NiveauUn {
 
             if (statut.equalsIgnoreCase("C")) {
                 tauxParticipation = TAUX_CELIB;
-            }
-            else {
+            } else {
                 tauxParticipation = TAUX_AUTRE;
             }
 
@@ -174,91 +170,312 @@ public class NiveauUn {
             }
             System.out.printf("Le taux de participation est de %.2f%%.%n", tauxParticipation);
 
-            // Étape 6 : Continuer ou arrêter
             System.out.print("Souhaitez-vous effectuer le calcul pour un autre employé ? (O/N) : ");
             continuer = scanner.next();
         }
         while (continuer.equalsIgnoreCase("O"));
 
 
-            scanner.close();
-            System.out.println("Fin du programme.");
-        }
+        scanner.close();
+        System.out.println("Fin du programme.");
+    }
 
-        public static void calculAbonnés() {
+    public static void calculAbonnés() {
 
         int nbAbonnés = 2500;
         int month = 0;
 
-        while(month < 24){
+        while (month < 24) {
             nbAbonnés *= 1.05;
             month += 1;
         }
 
-            int result =  (int) Math.round(nbAbonnés);
-            System.out.println("Le nombre d'abonnés est de : " + nbAbonnés);
+        int result = (int) Math.round(nbAbonnés);
+        System.out.println("Le nombre d'abonnés est de : " + nbAbonnés);
     }
 
-        public static int subscribersCount(int nbSbscribrs, double growth) {
+    public static int subscribersCount(int nbSbscribrs, double growth) {
 
-        for (int mois = 0; mois <24; mois++) {
+        for (int mois = 0; mois < 24; mois++) {
             nbSbscribrs *= growth;
         }
         int result = (int) Math.round(nbSbscribrs);
-            System.out.println("Votre nombre d'abonnés sera : " + result);
+        System.out.println("Votre nombre d'abonnés sera : " + result);
         return nbSbscribrs;
-        }
+    }
 
-        public static void sommeNb() {
-
-        Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Entrez un nombre : ");
-            int nbFinal = scanner.nextInt();
-            int somme = 0;
-            for (int i = 0; i < nbFinal; i++) {
-                somme += i;
-            }
-            System.out.println(somme);
-        }
-
-        public static void fizzBuzzing() {
+    public static void sommeNb() {
 
         Scanner scanner = new Scanner(System.in);
-            System.out.println("Entrez un nombre : ");
-            int n = scanner.nextInt();
 
-            for (int i = 1; i < n+1; i++) {
-                if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.println("Fizz");
-                } else if (i % 3 == 0) {
-                    System.out.println("Buzz");
-                } else if (i % 5 == 0 ) {
-                    System.out.println("Fizzbuzz");
-                }
-                else {
-                    System.out.println(i);
-                }
+        System.out.println("Entrez un nombre : ");
+        int nbFinal = scanner.nextInt();
+        int somme = 0;
+        for (int i = 0; i < nbFinal; i++) {
+            somme += i;
+        }
+        System.out.println(somme);
+    }
+
+    public static void fizzBuzzing() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez un nombre : ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 3 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            } else {
+                System.out.println(i);
             }
-
         }
 
-        public static long calculFactoriel(int n) {
+    }
+
+    public static long calculFactoriel(int n) {
 
         long f = 1;
 
-            for (int i = 1; i < n; i++) {
-                f *= n;
-            }
-            System.out.println(f);
-            return f;
+        for (int i = 1; i < n; i++) {
+            f *= n;
         }
+        System.out.println(f);
+        return f;
+    }
 
-        public static void tableauReel() {
+    public static void tableauReel() {
 
         double[] array = {12.2, 11.3, 4.4, 2.0};
-            System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int rechercheFor(int[] array, int value) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int rechercheWhile(int[] array, int value) {
+
+        int i = 0;
+        while (i < array.length) {
+            if (array[i] == value) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
+    public static int rechercheDoWhile(int[] array, int value) {
+
+        int i = 0;
+        do {
+            if (array[i] == value) {
+                return i;
+            }
+            i++;
+
+        } while (i < array.length);
+        return -1;
+    }
+
+    public static int sommeIndex(int[] array, int i, int j) {
+
+        int sum = 0;
+        for (int k = i; k < j; k++) {
+            sum += array[k];
+        }
+        return sum;
+    }
+
+    public static int[] findMinMaxIndexes(int[] array) {
+
+        int[] arrayResult = new int[2];
+
+        int indexMin = 0;
+        int indexMax = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[indexMin]) {
+                indexMin = i;
+            }
+            if (array[i] > array[indexMax]) {
+                indexMax = i;
+            }
+
+        }
+        arrayResult[0] = indexMin;
+        arrayResult[1] = indexMax;
+
+        return arrayResult;
+    }
+
+    public static boolean swap(int[] array, int i, int j) {
+
+        if (array == null || i < 0 || j < 0 || i > array.length || j > array.length) {
+            return false;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+        return true;
+    }
+
+    public static double calculerMoyenne(int[] array) {
+
+        int somme = 0;
+
+        for (int i : array) {
+            somme += i;
+        }
+        return (double) somme / array.length;
+    }
+
+    public static void tableManip() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Veuillez choisir un nombre de valeurs à saisir :");
+        int arraySize = scanner.nextInt();
+
+        int[] arrayToProcess = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            System.out.println("Veuillez saisir le nombre " + (i + 1) + " :");
+            arrayToProcess[i] = scanner.nextInt();
+        }
+        double moyenne = calculerMoyenne(arrayToProcess);
+        System.out.println("Moyenne = " + (int) moyenne);  // ou garde en double si tu veux la virgule
+
+        // Trouver min et max
+        int[] minMaxIndexes = findMinMaxIndexes(arrayToProcess);
+        int min = arrayToProcess[minMaxIndexes[0]];
+        int max = arrayToProcess[minMaxIndexes[1]];
+
+        System.out.println("Minimum = " + min);
+        System.out.println("Maximum = " + max);
+    }
+
+    public static int[] invert(int[] array) {
+
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[array.length - 1 - i];
+        }
+        System.out.println(Arrays.toString(newArray));
+        return newArray;
+    }
+
+    public static int sommeTableau2D(int[][] array2D) {
+
+        int sum = 0;
+
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) {
+                sum += array2D[i][j];
+            }
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static int calculateDiagonal(int[][] array2D, int width, int height, int diagonalType) {
+        int sum = 0;
+        int i = 0;
+
+        if (width != height) {
+            System.out.println("Erreur : le tableau n'est pas carré.");
+            return -1;
+        }
+
+        if (diagonalType == 1) {
+            while (i < height) {
+                sum += array2D[i][i];
+                i++;
+            }
+        } else if (diagonalType == 2) {
+            while (i < height) {
+                sum += array2D[i][width - 1 - i];
+                i++;
+            }
+        } else {
+            System.out.println("Erreur : type de diagonale invalide.");
+            return -1;
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static void doCharTriangle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez le nombre de lignes désirées :");
+        int enteredNumber = scanner.nextInt();
+        int i = 1;
+        boolean descending = false;
+
+        while (i <= enteredNumber) {
+            System.out.println("*".repeat(i));
+
+            if (!descending) {
+                i++;
+                if (i > enteredNumber) {
+                    descending = true;
+                    i -= 2;
+                }
+            } else {
+                i--;
+            }
+            if (i == 0) {
+                break;
+            }
         }
     }
+
+    public static void runPlusOuMoinsGame() {
+
+        Scanner scanner = new Scanner(System.in);
+        // instanciation d'un objet de la classe Random
+        Random r = new Random();
+        // génération d'un nombre >= 0 et < 100
+        int secretNumber = r.nextInt(100);
+
+        int nbTries = 10;
+
+        System.out.println("Essayez de deviner le nombre secret entre 1 et 100 : ");
+
+
+        for (int i = 0; i < nbTries; i++) {
+            int userEntry = scanner.nextInt();
+
+            if (userEntry < secretNumber) {
+                System.out.println("le nombre secret est plus grand");
+            }
+                else if (userEntry < 0 || userEntry > 100) {
+                    System.out.println("Le nombre entré n'est pas compris entre 1 et 100");
+                    continue;
+                } else if (userEntry > secretNumber) {
+                    System.out.println("le nombre secret est plus petit");
+                } else {
+                    System.out.println("Vous avez trouvé le nombre secret !");
+                    return;
+                }
+
+            }
+        }
+    }
+
 
 
